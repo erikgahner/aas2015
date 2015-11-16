68891c82b8fc32c8e084fddbf90d534f30867c19
Erik Gahner Larsen <erikgahner@gmail.com>
2015-11-16 23:32:45 +0100

    update R script for lab 6

--- lab-introR/lab6.r
+++ lab-introR/lab6.r
@@ -1,5 +1,5 @@
 # R script to lab session 6, "Introduction to R".
-# Available at https://github.com/erikgahner/aas2015
+# Also available at https://github.com/erikgahner/aas2015
 
 # As you can se, "#" in R is equivalent to "*" / "//" in Stata
 
@@ -7,8 +7,8 @@
 ## Highlight the line and press CTRL+R (Windows) or CMD+R (Mac)
 2+2             # Like "display 2+2" in Stata
 50*149
-3**2
-2**3
+3**2            # 3^2
+2**3            # 2^3
 sqrt(81)
 
 # R work with objects. We can save everything in objects.
@@ -16,35 +16,37 @@ sqrt(81)
 x <- 2  ## You can also type 2 -> x
 
 # Logical operations (you get TRUE or FALSE)
-x == 2
-x == 3
-x != 2
+x == 2        # Try to change the value of x above (e.g. to 3).
+x == 3        # "==" means "equal to"
+x != 2        # "!=" means "not equal to"
 x < 1
 x > 1
 x <= 2
 x >= 2.01
 
 # What class is x? (There are different object classes in R)
-class(x)
+class(x)      # x is a number, thus a numeric class
 
 # By the way, R is case sensitive
 class(X)  # x != X
 Class(X)  # Class() != class()
 
 # We can use our object for whatever we feel for
-x
-x + 2
+x       # show the value in the object
+x + 2   # the value of x plus 2
 
-x*x*x
+x*x*x   # x times x times x
 
 y <- x + 2    # We can create a new object with our object
-y
+y       # show the value of y
 
 # List numbers 1 to 10
-1:10
+1:10    # the same as c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) ... we get to c() in a moment
 
-# What do you think this will do?
-1:10 + 2
+# What do you think this will do? 
+## a) 1  2  3  4  5  6  7  8  9 10 2 
+## b) 3  4  5  6  7  8  9 10 11 12
+1:10 + 2  # Why? 
 
 # We can also combine numbers into vectors and lists
 c(2, 2)
@@ -63,15 +65,16 @@ x[2]
 # Can we remove the second number? Yes.
 x[-2]
 
-length(x)
-min(x)
-max(x)
-median(x)
-sum(x)
-mean(x)
-var(x)
+# We can get a lot of information
+length(x)     # the number of numbers in our vector
+min(x)        # the minimum value
+max(x)        # the maximum value
+median(x)     # the median
+sum(x)        # the sum
+mean(x)       # the mean
+var(x)        # the variance
 
-sd(x)
+sd(x)         # the standard deviation
 
 sqrt(var(x)) == sd(x) ## True dat.
 
@@ -84,11 +87,11 @@ mean(x)
 
 x <- c(x, NA)
 
-mean(x)
+mean(x)     # NA!?
 
 mean(x, na.rm=TRUE)
 
-mean(x, na.rm=T)
+mean(x, na.rm=T)    # T is an abbreviation for TRUE
 
 ?mean ### two marks question for online search: ??mean
 
@@ -100,9 +103,11 @@ z
 class(z)
 
 # We can add some more parties
-party <- c(z, "Enhedslisten", "SF", "Radikale", "Konservative", "Dansk Folkeparti", "Liberal Alliance", "Alternativet")
+party <- c(z, "Enhedslisten", "SF", "Radikale", "Konservative", 
+           "Dansk Folkeparti", "Liberal Alliance", "Alternativet")
 
-rw <- c(1, 0, 0, 0, 0, 1, 1, 1, 0)
+rw <- c(1, 0, 0, 0, 0, 1, 1, 1, 0)  # add info on whether the party is a right-wing party or not
+                                    # note the order, beginning with Venstre, ending with Alternativet
 
 party
 
@@ -110,11 +115,24 @@ vote <- c(19.5, 26.3, 7.8, 4.2, 4.6, 3.4, 21.1, 7.5, 4.8)
 
 seat <- c(34, 47, 14, 7, 8, 6, 37, 13, 9)
 
-# Create a data frame
+# Create a data frame (similar to a data set in Stata)
 pol <- data.frame(party, vote, seat, rw)
 
 # Show the data frame
 pol 
+
+# Show the data frame in a new window (good when working with large data frames)
+# Similar to "browse" in Stata
+View(pol)
+
+# Show the first five observations
+head(pol)
+
+# Show the first three observations
+head(pol, 3)
+
+# Show the last five observations
+tail(pol)
 
 # The class should be a data frame
 class(pol)
@@ -131,7 +149,7 @@ pol$party[pol$vote == max(pol$vote)]
 pol$party[pol$vote == min(pol$vote)]
 
 # What is the correlation between votes and seats? 
-cor(pol$vote, pol$seat) # Almost 1
+cor(pol$vote, pol$seat) # Wow.
 with(pol, cor(vote, seat))
 
 # We can even plot stuff
@@ -177,12 +195,13 @@ ggplot(pol, aes(x=vote, y=seat)) +
   xlab("Votes (%)") +
   geom_text(aes(label=ifelse(party == "Socialdemokraterne", "Social Democrats",''), hjust=1.1, vjust=0.5))
 
-
 # Let's "open" some data, i.e. load data into a data frame
+# It is important that you have the correct working directory
+getwd()     # You can use setwd() to change it
 # The data is simulated data from LaCour & Green (2014)
 lacour <- read.csv("lacoursciencedata.csv")
 
-# We can view the data, similar to "browse" in Stata
+# We can view the data
 View(lacour)
 
 # We subset the data to the observations used in Simulation 1
@@ -192,18 +211,19 @@ lacour <- subset(lacour, STUDY == "Study 1")
 # However, in R, we can have data frames in different objects at the same time
 pol   # See, still there.
 
-# Our data should be
+# Our data should be...
 class(lacour)
 
+# We can get descriptive statistics on all variables with summary()
 summary(lacour)
 
 # Does feelings toward gays affect support for same sex marriage?
-regression <- lm(SSM_Level ~ Therm_Level, data=lacour)  # In Stata: reg SSM_level Therm_Level
-summary(regression)
+reg.ssm <- lm(SSM_Level ~ Therm_Level, data=lacour)  # In Stata: reg SSM_level Therm_Level
+summary(reg.ssm)
 
 # Let's see if people interviewed in Wave 2 are more likely to have positive feelings toward gays
-regression <- lm(Therm_Change ~ Treatment_Assignment, data=lacour[lacour$wave==2 & lacour$Contact!="Secondary",])
-summary(regression)
+reg.therm <- lm(Therm_Change ~ Treatment_Assignment, data=lacour[lacour$wave==2 & lacour$Contact!="Secondary",])
+summary(reg.therm)
 
 # Last, we can see all we have
 ls()
